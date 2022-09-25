958fba0d11614599babc2939eff604fe0f030a14
Ernest Avagovich <erni1937@gmail.com>
2022-09-25 15:32:40 +0400

    Сделал ДЗ 4 тема: UISwitch

---
+++ UIKit_DZ_Swift2.0/UIKit_DZ_Swift2.0/RegistorGuestController.swift
@@ -0,0 +1,29 @@
+//
+//  RegistorGuestController.swift
+//  UIKit_DZ_Swift2.0
+//
+//  Created by Ernest Avagovich on 24.09.2022.
+//
+
+import UIKit
+
+class RegistorGuestController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
